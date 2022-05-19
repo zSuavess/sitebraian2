762ca7559dd44baa9e4415a4f8e6cd7d3f714452
zSuavess <31328731+zSuavess@users.noreply.github.com>
2022-05-18 22:37:46 -0300

    Add files via upload

---
+++ games.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Style.css">
+    <title>Games - FreeGames</title>
+</head>
+<body>
+
+    <div class="logo">
+                <a href="#"><img src="img/freegamesimagem.png" width="280px" /></a>
+    </div>
+
+    <nav>
+		<ul>
+            <li><a href="index.php">Início</a></li>
+			<li><a href="promocoes.php">Promoções</a></li>
+			<li><a class="active" href="games.php">Games</a></li>
+			<li><a href="sobrenos.php">Sobre nós</a></li>
+		</ul>
+    </nav>  
+	<div class="box">
+
+        <div class="box4">
+        <a href="#"><img src="#" width="" /></a>
+	    </div>
+
+        <div class="imgbox1"><a href="#"><img src="img/imagens capa site/imagem top 10 jogos.jpg" width="300px"/></a>
+        </div>
+	</div>
+
+    <div class="box2">
+        <a href="#"><img src="#" width="" /></a>
+	</div>
+
+    <div class="box3">
+        <a href="#"><img src="#" width="" /></a>
+	</div>
+
+    
+
+</body>
+</html>
